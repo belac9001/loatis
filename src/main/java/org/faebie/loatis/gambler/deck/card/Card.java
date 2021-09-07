90249ca8ca048cc20e3b901e8f5856f543c39998
galaeda <caleb.dederick@hotmail.com>
2021-09-07 14:56:53 -0700

    Change Card structure

--- src/main/java/org/faebie/loatis/gambler/deck/card/Card.java
+++ src/main/java/org/faebie/loatis/gambler/deck/card/Card.java
@@ -3,11 +3,14 @@ package org.faebie.loatis.gambler.deck.card;
 public class Card {
 
     private String name;
+    private String descriptionTitle;
     private String description;
 
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setDescriptionTitle(String descriptionTitle) { this.descriptionTitle = descriptionTitle; }
 
     public void setDescription(String description) {
         this.description = description;
@@ -15,6 +18,10 @@ public class Card {
 
     public String getName() {
         return this.name;
+    }
+
+    public String getDescriptionTitle() {
+        return this.descriptionTitle;
     }
 
     public String getDescription() {
