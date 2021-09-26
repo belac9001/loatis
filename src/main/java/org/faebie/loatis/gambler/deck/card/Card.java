8ec068ea836c6d0d872cb272345a47ec84bbf5b3
galaeda <caleb.dederick@hotmail.com>
2021-09-26 14:34:09 -0700

    Highlight current card choice

--- src/main/java/org/faebie/loatis/gambler/deck/card/Card.java
+++ src/main/java/org/faebie/loatis/gambler/deck/card/Card.java
@@ -2,9 +2,14 @@ package org.faebie.loatis.gambler.deck.card;
 
 public class Card {
 
+    private int id;
     private String name;
     private String descriptionTitle;
     private String description;
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -16,6 +21,10 @@ public class Card {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public String getName() {
